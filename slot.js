f736cd23e050c72edeea9845d72eae3828a677df
Saurabh Odhyan <saurabh.odhyan@yahoo.com>
2011-06-14 16:02:42 +0530

    Undo and fix last commit

--- slot.js
+++ slot.js
@@ -1,25 +1,60 @@
 /**
 * Slot machine
-* @author Saurabh Odhyan | http://odhyan.com
+* Author: Saurabh Odhyan | http://odhyan.com
+*
+* Licensed under the Creative Commons Attribution-ShareAlike License, Version 3.0 (the "License")
+* You may obtain a copy of the License at
+* http://creativecommons.org/licenses/by-sa/3.0/
+*
+* Date: May 23, 2011 
 */
 $(document).ready(function() {
     /**
     * Global variables
     */
-    var imgHeight = 1374,
-        posArr = [0, 80, 165, 237, 310, 378, 454, 539, 624, 696, 769, 837, 913, 1000, 1085, 1157, 1230, 1298];
+    var completed = 0,
+        imgHeight = 1374,
+        posArr = [
+            0, //orange
+            80, //number 7 
+            165, //bar
+            237, //guava
+            310, //banana
+            378, //cherry
+            454, //orange
+            539, //number 7
+            624, //bar
+            696, //guava
+            769, //banana
+            837, //cherry
+            913, //orange
+            1000, //number 7
+            1085, //bar
+            1157, //guava
+            1230, //banana
+            1298 //cherry
+        ];
     
+    var win = [];
+    win[0] = win[454] = win[913] = 1;
+    win[80] = win[539] = win[1000] = 2;
+    win[165] = win[624] = win[1085] = 3;
+    win[237] = win[696] = win[1157] = 4;
+    win[310] = win[769] = win[1230] = 5;
+    win[378] = win[837] = win[1298] = 6;
+
     /**
     * @class Slot
     * @constructor
     */
     function Slot(el, max, step) {
-        this.speed = 0;
-        this.step = step;
-        this.si = null;
-        this.el = el;
-        this.maxSpeed = max;
-        
+        this.speed = 0; //speed of the slot at any point of time
+        this.step = step; //speed will increase at this rate
+        this.si = null; //holds setInterval object for the given slot
+        this.el = el; //dom element of the slot
+        this.maxSpeed = max; //max speed this slot can have
+        this.pos = null; //final position of the slot    
+
         $(el).pan({
             fps:30,
             dir:'down'
@@ -73,6 +108,7 @@ $(document).ready(function() {
     */
     Slot.prototype.finalPos = function() {
         var el = this.el,
+            el_id,
             pos,
             posMin = 2000000000,
             best,
@@ -80,8 +116,9 @@ $(document).ready(function() {
             i,
             j,
             k;
-        
+
         el_id = $(el).attr('id');
+        //pos = $(el).css('background-position'); //for some unknown reason, this does not work in IE
         pos = document.getElementById(el_id).style.backgroundPosition;
         pos = pos.split(' ')[1];
         pos = parseInt(pos, 10);
@@ -93,6 +130,7 @@ $(document).ready(function() {
                     if((k - pos) < posMin) {
                         posMin = k - pos;
                         best = k;
+                        this.pos = posArr[i]; //update the final position of the slot
                     }
                     break;
                 }
@@ -104,7 +142,10 @@ $(document).ready(function() {
         $(el).animate({
             backgroundPosition:"(" + bgPos + ")"
         }, {
-            duration: 200
+            duration: 200,
+            complete: function() {
+                completed ++;
+            }
         });
     };
     
@@ -117,6 +158,8 @@ $(document).ready(function() {
         $._spritely.instances[el_id].t = 0;
         $(this.el).css('background-position', '0px 4px');
         this.speed = 0;
+        completed = 0;
+        $('#result').html('');
     };
 
     function enableControl() {
@@ -125,6 +168,16 @@ $(document).ready(function() {
 
     function disableControl() {
         $('#control').attr("disabled", true);
+    }
+
+    function printResult() {
+        var res;
+        if(win[a.pos] === win[b.pos] && win[a.pos] === win[c.pos]) {
+            res = "You Win!";
+        } else {
+            res = "You Lose";
+        }
+        $('#result').html(res);
     }
 
     //create slot objects
@@ -164,9 +217,10 @@ $(document).ready(function() {
             //check every 100ms if slots have stopped
             //if so, enable the control
             x = window.setInterval(function() {
-                if(a.speed == 0 && b.speed == 0 && c.speed == 0) {
+                if(a.speed === 0 && b.speed === 0 && c.speed === 0 && completed === 3) {
                     enableControl();
                     window.clearInterval(x);
+                    printResult();
                 }
             }, 100);
         } else { //reset
